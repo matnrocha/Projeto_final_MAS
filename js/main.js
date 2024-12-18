97bcb54e898fd5dbeb975382ed990f79163764b5
a16166 <bernardof.santos@ua.pt>
2024-12-18 18:23:09 +0000

    ja nem lembro

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 function loadComponent(componentId, url) {
-    $(document).ready(function(){
-      $(componentId).load(url);
+    document.addEventListener('DOMContentLoaded', function() {
+        const element = document.querySelector(componentId);
+        if (element) {
+            fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    element.innerHTML = data;
+                })
+                .catch(error => console.error('Error loading component:', error));
+        }
     });
 }
 
 // Load Navbar
 loadComponent('#navbar', 'components/navbar.html');
-  
+
 // Load Footer
 loadComponent('#footer', 'components/footer.html');
